d6ac3d27e90e4a066e70c274da0265231be85f46
Adam Edgett <AdamEdgett@gmail.com>
2013-01-10 12:35:49 -0500

    Finalized methods and added comments

--- Project 1/Mastermind.cpp
+++ Project 1/Mastermind.cpp
@@ -4,14 +4,16 @@ using namespace std;
 
 class code {
    public:
-       code(int n, int m);
+       code(int n = 4, int m = 4);
        void print() const;
-       int checkCorrect() const;
+       int checkCorrect(vector<int> guess) const;
     private:
         vector<int> data;
         int n, m;
 };
 
+// Constructor
+// Initializes a new code with n digits in length and 0-m as possible digits
 code::code(int newN, int newM) {
     n = newN;
     m = newM;
@@ -21,10 +23,17 @@ code::code(int newN, int newM) {
         data[i] = rand() % m;
     }
 }
+// Checks the user's guess and returns the number of spots that are correct
 int code::checkCorrect(vector<int> guess) const {
-    return 0;
+    int numCorrect = 0;
+    for(int i = 0; i < n; i++) {
+        if(data[i] == guess[i]) {
+            numCorrect++;
+        }
+    }
+    return numCorrect;
 }
-
+// Prints the code
 void code::print() const {
     cout << "data:" << endl;
     for(unsigned int i = 0; i < data.size(); i++) {
@@ -39,6 +48,21 @@ int main () {
     cin >> n;
     cout << "Enter the range of digits (<=10)" << endl;
     cin >> m;
-    Mastermind master (n, m);
+    code master (n, m);
     master.print();
+    //prompt user for guesses
+    for(int i = 0; i < 4; i++) {
+        vector<int> guess(n);
+        cout << "Enter a guess" << endl;
+        for(int i = 0; i < n; i++) {
+            cin >> guess[i];
+        }
+        int result = master.checkCorrect(guess);
+        if(result == n) {
+            cout << "Correct!" << endl;
+            return 0;
+        } else {
+            cout << "You got " << result << " correct" << endl;
+        }
+    }
 }
