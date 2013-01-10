d089f0bde12d8fde2d8e6f2087673319af4b4783
Adam Edgett <AdamEdgett@gmail.com>
2013-01-10 08:36:09 -0500

    Added skeleton for Project 1

---
+++ Project 1/Mastermind.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Mastermind {
+   public:
+        
+   private:
+        
+};
+
+Mastermind::Mastermind(int n, int m) {
+    
+}
+int Mastermind::checkCorrect() const {
+    
+}
+
+int main () {
+    
+}
