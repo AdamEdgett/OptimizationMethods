d1f98d199e31d023c113f38bfabb2b987a08f038
Adam Edgett <AdamEdgett@gmail.com>
2013-01-12 13:35:40 -0500

    Separated code class into a class file

--- Project 1/code.h
+++ Project 1/code.h
@@ -1,3 +1,6 @@
+// Adam Edgett & Dan Nordness
+// Project 1
+// code.h
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -23,6 +26,7 @@ code::code(int newN, int newM) {
         data[i] = rand() % m;
     }
 }
+
 // Checks the user's guess and returns the number of spots that are correct
 int code::checkCorrect(vector<int> guess) const {
     int numCorrect = 0;
@@ -33,36 +37,12 @@ int code::checkCorrect(vector<int> guess) const {
     }
     return numCorrect;
 }
+
 // Prints the code
 void code::print() const {
-    cout << "data:" << endl;
+    cout << "Answer:" << endl;
     for(unsigned int i = 0; i < data.size(); i++) {
         cout << data[i] << " ";
     }
     cout << endl;
-}
-
-int main () {
-    int n, m;
-    cout << "Enter the number of digits to use:" << endl;
-    cin >> n;
-    cout << "Enter the range of digits (<=10)" << endl;
-    cin >> m;
-    code master (n, m);
-    master.print();
-    //prompt user for guesses
-    for(int i = 0; i < 4; i++) {
-        vector<int> guess(n);
-        cout << "Enter a guess" << endl;
-        for(int i = 0; i < n; i++) {
-            cin >> guess[i];
-        }
-        int result = master.checkCorrect(guess);
-        if(result == n) {
-            cout << "Correct!" << endl;
-            return 0;
-        } else {
-            cout << "You got " << result << " correct" << endl;
-        }
-    }
 }
---
+++ Project 1/program.cpp
@@ -0,0 +1,35 @@
+// Adam Edgett & Dan Nordness
+// Project 1
+// program.cpp
+#include <iostream>
+#include <vector>
+#include "code.h"
+using namespace std;
+
+int main () {
+    // Setup for game
+    int n, m;
+    cout << "Enter the number of digits to use:" << endl;
+    cin >> n;
+    cout << "Enter the range of digits (<=10)" << endl;
+    cin >> m;
+    code master (n, m);
+    master.print();
+    
+    // Prompt user for guesses
+    for(int i = 0; i < 4; i++) {
+        vector<int> guess(n);
+        cout << "Enter a guess" << endl;
+        for(int i = 0; i < n; i++) {
+            cin >> guess[i];
+        }
+        // Check this guess
+        int result = master.checkCorrect(guess);
+        if(result == n) {
+            cout << "Correct!" << endl;
+            return 0;
+        } else {
+            cout << "You got " << result << " correct" << endl;
+        }
+    }
+}
