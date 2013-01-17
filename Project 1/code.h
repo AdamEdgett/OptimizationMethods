82d8fcb197bda0906b8548a6baf2f68f1186ad42
Daniel Nordness <Daniel0524@gmail.com>
2013-01-17 12:47:50 -0500

    Fixed Adam\'s terrible terrible terrible algorithm in code::incorrect

--- Project 1/code.h
+++ Project 1/code.h
@@ -41,19 +41,32 @@ int code::checkCorrect(vector<int> guess) const {
 
 //Check for correct digits in the incorrect place
 int code::checkIncorrect(vector<int> guess) const {
-    int numMisplaced = 0;
-    vector<int> dataCopy = data;
-    for(int i = 0; i < n; i++) {
-        if(dataCopy[i] == guess[i]) {
-            dataCopy[i] = -1;
-        }
-        for(int j = 0; j < n; j++) {
-            if(i != j && dataCopy[i] == guess[j]) {
-                numMisplaced++;
-                dataCopy[i] = -1;
-            }
-        }
-    }
+    int numMisplaced = 0; //Start at zero
+    vector<int> dataCopy = data; //Make copy of the secret code (because we'll be changing the data inside the vector and therefore can't use original)
+	vector<int> guessCopy = guess;
+    for(int i = 0; i < n; i++) 
+	{ //Go through the top number and see if they are the same index with same value AKA number corrent
+	  	if(dataCopy[i] == guessCopy[i]) 
+		{ //If they are
+            dataCopy[i] = -1; //Change the code value at i to -1 so that it won't be considered in the number misplaced counter
+			guessCopy[i]	= -1; //Change the guess value at i to -1 for the same reason
+	    }
+	}
+	for (int i = 0; i < n; i++) //Go through dataCopy, and stop to look for a misplaced value at all indices whose values !=-1
+	{
+		if (dataCopy[i]!=-1)
+		{
+	  	 	for(int j = 0; j < n; j++) //Iterate through guess and see if there are misplaced values
+			{
+	           if(dataCopy[i] == guessCopy[j]) //If there is one misplaced
+				{
+	               	numMisplaced++; //add one
+					dataCopy[i] = -1; //flag both so they're not longer considered
+					guessCopy[i] 	= -1;
+		        }
+			}
+		}
+  	}
     return numMisplaced;
 }
 
