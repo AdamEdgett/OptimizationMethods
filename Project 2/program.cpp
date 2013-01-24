086d4a519e7a53edd36de1611052be44f74d4693
Adam Edgett <AdamEdgett@gmail.com>
2013-01-24 12:32:04 -0500

    Added main program file

---
+++ Project 2/program.cpp
@@ -0,0 +1,14 @@
+#include "deck.h";
+using namespace std;
+
+// card c(1, "HEARTS");
+// card c2(5, "CLUBS");
+// node<card> *n, *n2;
+// n2 = new node<card>(c2);
+// n = new node<card>(c, n2);
+// setFirst(n);
+
+int main() {
+    deck d;
+    cout << d << endl;
+}
