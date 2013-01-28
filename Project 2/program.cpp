58e4bdf49746eb3770ad2821fff1225848b50ea2
Daniel Nordness <Daniel0524@gmail.com>
2013-01-27 23:23:40 -0500

    Added error catching kind of, we dont really take any input at the moment so I dont understand where Id be catching errors. Maybe a range exception? I didnt actually throw any just try and catch at the moment.

--- Project 2/program.cpp
+++ Project 2/program.cpp
@@ -1,10 +1,18 @@
 #include <cstdlib>
 #include "deck.h"
+#include "d_except.h"
 using namespace std;
 
 
 
 int main() {
-    deck d;
-    cout << d << endl;
+    try {
+     deck d;
+     cout << d << "\n";
+    }
+    catch (rangeError e) {
+    cout << "There was an error with the selected value or suit, perhaps out of range. \n";
+    cout << e.what() << "\n";
+    return(0);
+    }
 }
