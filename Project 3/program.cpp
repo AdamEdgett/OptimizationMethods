c0ae58d1f48f4661d9de439c020c2acc0de6784d
Daniel Nordness <Daniel0524@gmail.com>
2013-02-07 13:14:46 -0500

    Built Grid Class

--- Project 3/program.cpp
+++ Project 3/program.cpp
@@ -1,10 +1,31 @@
 #include <string>
 #include <vector>
 #include "wordlist.h"
+#include "grid.h"
+
 
 using namespace std;
 
+void findMatches(wordlist w, grid g)
+{
+	for (int i=0; i<g.crossword.cols();i++) {
+		for (int j=0; j<g.crossword.rows(); j++){
+			cout<< " Value= "<< g.crossword[i][j]<<"\n";
+		}
+	}
+}
+
+const char* stringToChar(string s){
+	const char* c=s.c_str();
+	return c;
+}
+
 int main(void)
 {
+	const char* c = stringToChar("Dog");
+	
+	cout <<c[0]<<"\n";
 	wordlist test;
+	grid g("input15");
+	findMatches(test,g);
 }
--- Project 3/wordlist.h
+++ Project 3/wordlist.h
@@ -6,56 +6,48 @@ using std::string;
 
 
 
-class wordlist
-{
-public:
-	wordlist():wlist(NULL)
-		{
-			std::cout <<"Reading\n";
-			fname="wordlist";
-			readfile();
-		}
-	wordlist(string fn):wlist(NULL)
-	{
-		fname=fn;
-		readfile();
-	}
-	void readfile();
-	string fname;
-private:
-	vector<string> wlist;	
+class wordlist {
+	public:
+		wordlist();
+		wordlist(string fn);
+		void readFile();
+		string fname;
+	private:
+		vector<string> wlist;	
 };
 
-void wordlist::readfile()
-{
+
+wordlist::wordlist():wlist(NULL) {
+	std::cout <<"Reading\n";
+	fname="wordlist";
+	readFile();
+}
+wordlist::wordlist(string fn):wlist(NULL) {
+	fname=fn;
+	readFile();
+}
+
+void wordlist::readFile() {
 	std::cout <<"Reading in '"<<fname<<"'. \n";
 	std::fstream file;
 	
 	file.open(fname.c_str(),std::ios::in);
-	if (!file)
-	{
+	if (!file) {
 		std::cout << "Can't open, Bro.\n";
 	}
 
 	char wordChar[50];
 	string wordString;
 	int wordCount=0;
-	while(!file.eof())
-	{	
+	while(!file.eof()) {	
 		file.getline(wordChar,50);
 		wordString=wordChar;
-		if (wlist.empty())
-		{
+		if (wordString!="") {
 			wlist.push_back(wordString);
+			wordCount++;
 		}
-		else
-		{
-			wlist.push_back(wordString);
-		}
-		wordCount++;
-//		std::cout << wordString << "\n";
 	}
-	std::cout << "The word count is "<<wordCount<<"\n";
+	std::cout << "The word count is "<<wordCount<<" and the vector size is "<<wlist.size()<<"\n";
 	std::cout << "The first word is "<<wlist.front()<<"\n";
 	std::cout << "The last word is "<<wlist.back()<<"\n";
 }
