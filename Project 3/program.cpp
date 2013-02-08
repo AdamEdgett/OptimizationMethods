c724c6dd9c70fdbd1b24a3e7895485e9d0080ae7
Daniel Nordness <Daniel0524@gmail.com>
2013-02-07 21:10:11 -0500

    Iterates through the grid properly. Right now all it does is print the direction and list the whole string from start to beginnging. Had to use my own implementation of modulus since C++ doesnt support negative inputs properly. Snow Day Tomorrow. Not sure how I feel about that since it means work is cancelled and I kind of need the money, well I guess Ill try to finish this tomorrow then. I hshould call todd.

--- Project 3/program.cpp
+++ Project 3/program.cpp
@@ -2,15 +2,102 @@
 #include <vector>
 #include "wordlist.h"
 #include "grid.h"
-
+#include <cmath>
 
 using namespace std;
 
-void findMatches(wordlist w, grid g)
+/*
+class wordNode
 {
-	for (int i=0; i<g.crossword.cols();i++) {
-		for (int j=0; j<g.crossword.rows(); j++){
-			cout<< " Value= "<< g.crossword[i][j]<<"\n";
+	public:
+		wordNode();
+		wordNode(char v){
+			value=v;
+		}
+		char value;
+		wordNode * next;
+		wordNode * prev;
+};
+
+class wordLoop {
+	public:
+		wordLoop(const grid& g, int x, int y):first(g.crossword[x][y])
+		{
+			wordNode * iter =
+			for (int i=1;i<x+1;i++){
+				first.next=new wordNode(g.crossword[x+i][y]);		
+				first.next->prev=&first;
+			}
+		}
+		wordNode  first;
+};
+*/
+int mod(int number, int modulus)
+{
+     int result = number % modulus;
+
+     if (result < 0) result += modulus;
+
+     return result;
+}
+
+void getAllWords(const grid * g,const wordlist * w,int x_pos,int  y_pos){
+	int length=g->crossword.cols();
+	for (int i=-1;i<2;i++)
+	{
+		for (int j=-1;j<2;j++)
+		{	
+			if (i==-1 && j==-1)
+			{
+				cout << "\nNorth West: \n";
+			}
+			if (i==-1 && j==0)
+			{
+				cout << "\nWest: \n";
+			}
+			if (i==-1 && j==1)
+			{
+				cout << "\nSouth West: \n";
+			}
+			if (i==0 && j==-1)
+			{
+				cout << "\nNorth: \n";
+			}
+			if (i==0 && j==1)
+			{
+				cout << "\nSouth: \n";
+			}
+			if (i==1 && j==-1)
+			{
+				cout << "\nNorth East: \n";
+			}
+			if (i==1 && j==0)
+			{
+				cout << "\nEast: \n";
+			}
+			if (i==1 && j==1)
+			{
+				cout << "\nSouth East: \n";
+			}
+			
+			for (int k=0;k<length;k++) 
+			{
+				
+				if (i!=0 || j!=0)
+				{
+					cout << g->crossword[mod((x_pos+(k*i)),length)][mod(y_pos+(k*j),length)] << " ";	
+				}
+			}
+		}
+	}
+	cout << "\n";
+}
+
+void findMatches(const wordlist * w,const grid *g){
+	for (int i=0; i<g->crossword.cols();i++) {
+		for (int j=0; j<g->crossword.rows(); j++) {
+			cout<< "\nLetter = "<< g->crossword[j][i]<<" at point "<<j<<","<<i<<"\n";
+			getAllWords(g,w,j,i);
 		}
 	}
 }
@@ -25,7 +112,7 @@ int main(void)
 	const char* c = stringToChar("Dog");
 	
 	cout <<c[0]<<"\n";
-	wordlist test;
-	grid g("input15");
-	findMatches(test,g);
+	wordlist * w=new wordlist;
+	grid * g=new grid("input15");
+	findMatches(w,g);
 }
