ab8f42a6e00a987cb401ef3cddbbfd0b808e86b6
Adam Edgett <AdamEdgett@gmail.com>
2013-02-21 12:40:05 -0500

    Added setCell and isBlank functions. Fixed indentation

--- Project 4/p4.cpp
+++ Project 4/p4.cpp
@@ -27,20 +27,20 @@ int numSolutions = 0;
 class board
 // Stores the entire Sudoku board
 {
-   public:
-      board(int);
-      void clear();
-      void initialize(ifstream &fin);
-      void print();
-      bool isBlank(int, int);
-      ValueType getCell(int, int);
-      
-   private:
+    public:
+        board(int);
+        void clear();
+        void initialize(ifstream &fin);
+        void print();
+        bool isBlank(int, int);
+        ValueType getCell(int, int);
+        void setCell(int, int, ValueType);
+    private:
 
-      // The following matrices go from 1 to BoardSize in each
-      // dimension.  I.e. they are each (BoardSize+1) X (BoardSize+1)
+        // The following matrices go from 1 to BoardSize in each
+        // dimension.  I.e. they are each (BoardSize+1) X (BoardSize+1)
 
-      matrix<ValueType> value;
+        matrix<ValueType> value;
 };
 
 board::board(int sqSize)
@@ -53,123 +53,133 @@ void board::clear()
 {
 }
 
+void board::setCell(int i, int j, ValueType cellVal)
+{
+    if (i < 1 || i > BoardSize || j < 1 || j > BoardSize)
+        throw rangeError("bad value in setCell");
+    else
+        value[i][j] = cellVal;
+}
+
 void board::initialize(ifstream &fin)
 // Read a Sudoku board from the input file.
 {
-   char ch;
+    char ch;
 
-   clear();
-   for (int i = 1; i <= BoardSize; i++)
-      for (int j = 1; j <= BoardSize; j++)
-      {
-	 fin >> ch;
+    clear();
+    for (int i = 1; i <= BoardSize; i++)
+        for (int j = 1; j <= BoardSize; j++)
+        {
+            fin >> ch;
 
-         // If the read char is not Blank
-	 if (ch != '.')
-	 {
-            setCell(i,j,ch-'0');   // Convert char to int
-	 }
-      }
+            // If the read char is not Blank
+            if (ch != '.')
+            {
+                setCell(i,j,ch-'0');   // Convert char to int
+            }
+        }
 }
 
 int squareNumber(int i, int j)
 // Return the square number of cell i,j (counting from left to right,
 // top to bottom.  Note that i and j each go from 1 to BoardSize
 {
-   // Note that (int) i/SquareSize and (int) j/SquareSize are the x-y
-   // coordinates of the square that i,j is in.  
+    // Note that (int) i/SquareSize and (int) j/SquareSize are the x-y
+    // coordinates of the square that i,j is in.  
 
-   return SquareSize * ((i-1)/SquareSize) + (j-1)/SquareSize + 1;
+    return SquareSize * ((i-1)/SquareSize) + (j-1)/SquareSize + 1;
 }
 
 ostream &operator<<(ostream &ostr, vector<int> &v)
 // Overloaded output operator for vector class.
 {
-   for (int i = 0; i < v.size(); i++)
-      ostr << v[i] << " ";
-   ostr << endl;
-   return ostr;
+    for (unsigned int i = 0; i < v.size(); i++)
+        ostr << v[i] << " ";
+    ostr << endl;
+    return ostr;
 }
 
 ValueType board::getCell(int i, int j)
 // Returns the value stored in a cell.  Throws an exception
 // if bad values are passed.
 {
-   if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize)
-      return value[i][j];
-   else
-      throw rangeError("bad value in getCell");
+    if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize)
+        return value[i][j];
+    else
+        throw rangeError("bad value in getCell");
 }
 
 bool board::isBlank(int i, int j)
 // Returns true if cell i,j is blank, and false otherwise.
 {
-   if (i < 1 || i > BoardSize || j < 1 || j > BoardSize)
-      throw rangeError("bad value in setCell");
+    if (i < 1 || i > BoardSize || j < 1 || j > BoardSize)
+        throw rangeError("bad value in setCell");
+    else
+        return !getCell(i,j);
 }
 
 void board::print()
 // Prints the current board.
 {
-   for (int i = 1; i <= BoardSize; i++)
-   {
-      if ((i-1) % SquareSize == 0)
-      {
-         cout << " -";
-	 for (int j = 1; j <= BoardSize; j++)
-	    cout << "---";
-         cout << "-";
-	 cout << endl;
-      }
-      for (int j = 1; j <= BoardSize; j++)
-      {
-	 if ((j-1) % SquareSize == 0)
-	    cout << "|";
-	 if (!isBlank(i,j))
-	    cout << " " << getCell(i,j) << " ";
-	 else
-	    cout << "   ";
-      }
-      cout << "|";
-      cout << endl;
-   }
+    for (int i = 1; i <= BoardSize; i++)
+    {
+        if ((i-1) % SquareSize == 0)
+        {
+            cout << " -";
+            for (int j = 1; j <= BoardSize; j++)
+                cout << "---";
+            cout << "-";
+            cout << endl;
+        }
+        for (int j = 1; j <= BoardSize; j++)
+        {
+            if ((j-1) % SquareSize == 0)
+                cout << "|";
+            if (!isBlank(i,j))
+                cout << " " << getCell(i,j) << " ";
+            else
+                cout << "   ";
+        }
+        cout << "|";
+        cout << endl;
+    }
 
-   cout << " -";
-   for (int j = 1; j <= BoardSize; j++)
-      cout << "---";
-   cout << "-";
-   cout << endl;
+    cout << " -";
+    for (int j = 1; j <= BoardSize; j++)
+        cout << "---";
+    cout << "-";
+    cout << endl;
 }
 
 int main()
 {
-   ifstream fin;
+    ifstream fin;
    
-   // Read the sample grid from the file.
-   string fileName = "sudoku.txt";
+    // Read the sample grid from the file.
+    string fileName = "sudoku1.txt";
 
-   fin.open(fileName.c_str());
-   if (!fin)
-   {
-      cerr << "Cannot open " << fileName << endl;
-      exit(1);
-   }
+    fin.open(fileName.c_str());
+    if (!fin)
+    {
+        cerr << "Cannot open " << fileName << endl;
+        exit(1);
+    }
 
-   try
-   {
-      board b1(SquareSize);
+    try
+    {
+        board b1(SquareSize);
 
-      while (fin && fin.peek() != 'Z')
-      {
-	 b1.initialize(fin);
-	 b1.print();
-	 b1.printConflicts();
-      }
-   }
-   catch  (indexRangeError &ex)
-   {
-      cout << ex.what() << endl;
-      exit(1);
-   }
+        while (fin && fin.peek() != 'Z')
+        {
+            b1.initialize(fin);
+            b1.print();
+            //b1.printConflicts();
+        }
+    }
+    catch  (indexRangeError &ex)
+    {
+        cout << ex.what() << endl;
+        exit(1);
+    }
 }
 
